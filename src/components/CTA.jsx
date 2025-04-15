952910c77d1366763894ae09d515a7a186a4b8d3
Aries <luisaries1998@gmail.com>
2025-04-15 12:22:11 -0600

    modify styles and add buttons

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,4 +1,5 @@
 import '../styles/CTA.css'
+import { Link } from 'react-router-dom';
 
 const CTA = () => {
   return (
@@ -6,7 +7,10 @@ const CTA = () => {
       <div className="cta-content">
         <h2>¿Listo para comenzar?</h2>
         <p>Comunicate ya y obten un presupuesto sin costos ni compromisos</p>
-        <button className="cta-button">Contactar</button>
+        <Link to="/contact">
+          <button className="cta-button">Contactar</button>
+        </Link>
+        
       </div>
     </section>
   )
--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -9,8 +9,29 @@ const ContactPage= () => {
   
 
   return (
-    <>
-   <h1 >   pagina de contacto    </h1>
+    <><div className='contact-container'>
+        <h1 >   pagina de contacto    </h1>
+        <div className='contact-info'>
+          <p>Correo: shorelinebcs@gmail.com</p>
+          <p>location: Pescader, La Paz, BCS, Mexico.</p>
+          <p>tel: 612 123 4567</p>
+        </div>
+        <div className='contact-form'>
+          <h2>Contact Us</h2>
+          <form action="https://formspree.io/f/xjvjdqzq" method="POST">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+
+            <button type="submit">Send</button>
+          </form>
+        </div>
+    </div>
     <Link to="/">
           <button className="back-btn">Home</button>
     </Link>
